4ddece1bd8a48ec6aeabd25c1770995edcf5d875
Zhenyu Lin <zhenyu.lyn@gmail.com>
2017-03-21 14:38:48 +0000

    linting

--- server/handlers/scrape/sample.js
+++ server/handlers/scrape/sample.js
@@ -8,20 +8,14 @@ import Keyword from 'server/models/keyword';
 const START = 'https://www.ebay.co.uk';
 // let INDEX = 0;
 
-export default function(){
-	console.log('scraper started');
-	scrapeNext();
-}
-
-export const scrapeNext = async () => {
-	// const keyword = TEST_KEYWORDS[INDEX];
-	const result = await Keyword.findOneAndUpdate({scraped: false}, {scraped: true});
-	if (result === null) { return console.log('no more unscraped keyword'); }
-	const { keyword } = result;
-	await scrapeKeyword(keyword);
-	scrapeNext();
-	// INDEX++;
-	// INDEX < TEST_KEYWORDS.length ? scrapeNext() : console.log('No more unscraped keyword');
+export const extractSample = (row, keyword) => {
+	const item = row.find('h3.lvtitle').text().trim();
+	const priceText = row.find('li.lvprice').text().trim().split(' ')[0].split('\n')[0];
+	const price = parsePrice(priceText);
+	const typeText = row.find('li.lvformat').text().trim();
+	const type = typeText.length ? 'Bidding' : 'Buy It Now';
+	const sample = new Sample({ keyword, price, type, item });
+	return sample.save();
 };
 
 export const scrapeKeyword = keyword => new Promise(async (resolve, reject) => {
@@ -49,12 +43,18 @@ export const scrapeKeyword = keyword => new Promise(async (resolve, reject) => {
 	}
 });
 
-export const extractSample = (row, keyword) => {
-	const item = row.find('h3.lvtitle').text().trim();
-	const priceText = row.find('li.lvprice').text().trim().split(' ')[0].split('\n')[0];
-	const price = parsePrice(priceText);
-	const typeText = row.find('li.lvformat').text().trim();
-	const type = typeText.length ? 'Bidding' : 'Buy It Now';
-	const sample = new Sample({ keyword, price, type, item });
-	return sample.save();
+export const scrapeNext = async () => {
+	// const keyword = TEST_KEYWORDS[INDEX];
+	const result = await Keyword.findOneAndUpdate({scraped: false}, {scraped: true});
+	if (result === null) { return console.log('no more unscraped keyword'); }
+	const { keyword } = result;
+	await scrapeKeyword(keyword);
+	scrapeNext();
+	// INDEX++;
+	// INDEX < TEST_KEYWORDS.length ? scrapeNext() : console.log('No more unscraped keyword');
 };
+
+export default function(){
+	console.log('scraper started');
+	scrapeNext();
+}
